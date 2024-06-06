cd226c72b762214e49feb47fe17f2c349312ea0f
Loïc Gremaud <grelinfo@gmail.com>
2024-06-06 06:01:14 +0100

    Deployed e32f04f to 0.0 with MkDocs 1.6.0 and mike 2.1.1

---
+++ 0.0/src/getting_started/tutorial001.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi_apscheduler4 import SchedulerApp
+
+scheduler_app = SchedulerApp()
+app = FastAPI(lifespan=scheduler_app.lifespan)
+
+
+@scheduler_app.interval(seconds=5)
+def say_hello_world() -> None:
+    print("test1")
